fde9c8f8b505af1fbdc3c81aaadbb5f73005bb0d
sitopga <sitopga@gmail.com>
2024-01-10 22:32:49 +0100

    cambio a carpetas por seguridas

---
+++ src/main.js
@@ -0,0 +1,42 @@
+// main.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, signInWithEmailAndPassword, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCyHr3QmcPsP8BjeUpZLPQ6FkjlLB_v7JU",
+    authDomain: "db-tecnicusuarios.firebaseapp.com",
+    projectId: "db-tecnicusuarios",
+    storageBucket: "db-tecnicusuarios.appspot.com",
+    messagingSenderId: "500178473738",
+    appId: "1:500178473738:web:916af8899ffcef0a5420f3"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+        console.log("Usuario autenticado:", user);
+    } else {
+        console.log("Usuario no autenticado. Redirigiendo a la página de inicio de sesión.");
+    }
+});
+
+document.getElementById('login-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+            console.log("Usuario autenticado:", user);
+            window.location.href = './inicio.html';
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("Error de autenticación:", errorCode, errorMessage);
+        });
+});
